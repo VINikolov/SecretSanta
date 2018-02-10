e53bbe70266747ba35904bff00aa419c6ff87f3b
Vasil Nikolov <v.nikolov.95@gmail.com>
2018-02-10 11:54:34 +0200

    Implemented endpoint for inviting a user to a group.

--- SecretSanta/Controllers/GroupsController.cs
+++ SecretSanta/Controllers/GroupsController.cs
@@ -3,7 +3,9 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
 using BusinessLogic.Interfaces;
+using Models.ApiResponseModels;
 using Models.DataTransferModels;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace SecretSanta.Controllers
@@ -11,11 +13,13 @@ namespace SecretSanta.Controllers
     public class GroupsController : ApiController
     {
         private readonly IGroupsManager _groupsManager;
+        private readonly IInvitationsManager _invitationsManager;
         private User _currentUser;
-
-        public GroupsController(IGroupsManager groupsManager)
+        
+        public GroupsController(IGroupsManager groupsManager, IInvitationsManager invitationsManager)
         {
             _groupsManager = groupsManager;
+            _invitationsManager = invitationsManager;
         }
 
         [HttpPost]
@@ -29,6 +33,21 @@ namespace SecretSanta.Controllers
             var json = JObject.Parse(jsonString).ToString();
 
             return new HttpResponseMessage(HttpStatusCode.Created) { Content = new StringContent(json) };
+        }
+
+        [HttpPost]
+        [Route("api/groups/{groupname}/invitations")]
+        public async Task<HttpResponseMessage> SendInvitation([FromBody]GroupInvitation invitation, [FromUri] string groupName)
+        {
+            invitation.GroupName = groupName;
+            var invitationId = await _invitationsManager.CreateInvitation(invitation);
+            var invitationResponse = new GroupInvitationResponse
+            {
+                Id = invitationId
+            };
+
+            var jsonContent = JsonConvert.SerializeObject(invitationResponse);
+            return new HttpResponseMessage(HttpStatusCode.Created) { Content = new StringContent(jsonContent) };
         }
 
         public void SetCurrentUser(User user)
